4e1878846b8f131273bcb21ee7ea242c2fc7ac94
sa-keisuke1230 <saito.keisuke1230@gmail.com>
2025-06-22 23:07:55 +0900

    random generate

--- reversi/reversiManeger.cs
+++ reversi/reversiManeger.cs
@@ -12,6 +12,7 @@ namespace reversi
         public cellStatus[,] cellStatusList;
         public Size FIELD_SIZE = new Size(8, 8);
         public cellStatus currentP;
+        Random rnd = new Random();
 
         public void InitilizeBoard()
         {
@@ -24,11 +25,20 @@ namespace reversi
                 }
             }
 
+            for(int x = 0; x < FIELD_SIZE.Width; x++)
+            {
+                cellStatusList[rnd.Next(0, 7), rnd.Next(0, 7)] = cellStatus.BLACK;
+            }
+
+            for (int x = 0; x < FIELD_SIZE.Width; x++)
+            {
+                cellStatusList[rnd.Next(0, 7), rnd.Next(0, 7)] = cellStatus.WHITE;
+            }
+
             cellStatusList[0, 0] = cellStatus.BLACK;
             cellStatusList[3, 4] = cellStatus.BLACK;
             cellStatusList[4, 3] = cellStatus.BLACK;
             cellStatusList[2, 5] = cellStatus.BLACK;
-
 
             cellStatusList[2, 4] = cellStatus.WHITE;
             cellStatusList[1, 0] = cellStatus.WHITE;
@@ -37,6 +47,12 @@ namespace reversi
             cellStatusList[4, 4] = cellStatus.WHITE;
             cellStatusList[5, 4] = cellStatus.WHITE;
             cellStatusList[6, 4] = cellStatus.WHITE;
+            cellStatusList[2, 5] = cellStatus.WHITE;
+            cellStatusList[2, 4] = cellStatus.WHITE;
+            cellStatusList[2, 6] = cellStatus.BLACK;
+
+            cellStatusList[2, 3] = cellStatus.WHITE;
+            cellStatusList[2, 2] = cellStatus.WHITE;
 
             currentP = cellStatus.BLACK;
         }
@@ -92,7 +108,7 @@ namespace reversi
             // 右
             playerBorad = (int[,])black_borad.Clone();
             enemyBorad = (int[,])white_borad.Clone();
-            for (int count = 0; count < FIELD_SIZE.Width - 2; count++)
+            for (int count = 0; count < FIELD_SIZE.Width - 3; count++)
             {
                 for (int y = 0; y < FIELD_SIZE.Height; y++)
                 {
@@ -110,7 +126,7 @@ namespace reversi
                     for (int x = 0; x < FIELD_SIZE.Width; x++)
                     {
                         if (tmp_borad[x, y] == 1) continue;
-                        tmp_borad[x, y] = ((playerBorad[x, y] == 1 && enemyBorad[x, y] == 1) && x < FIELD_SIZE.Width - 1) ? 1 : 0;
+                        tmp_borad[x, y] = ((playerBorad[x, y] == 1 && enemyBorad[x, y] == 1) && x < FIELD_SIZE.Width - 2 && x > 0) ? 1 : 0;
                     }
                 }
             }
@@ -137,6 +153,7 @@ namespace reversi
 
             
             // 左
+            /*
             playerBorad = (int[,])black_borad.Clone();
             enemyBorad = (int[,])white_borad.Clone();
             tmp_borad = new int[FIELD_SIZE.Height, FIELD_SIZE.Width];
@@ -181,11 +198,12 @@ namespace reversi
                     if (legal_borad[x, y] == 1) continue;
                     legal_borad[x, y] = (tmp_borad[x, y] == 1 && empty_borad[x, y] == 1) ? 1 : 0;
                 }
-            }
+            }*/
             
 
            
             // 上
+            /*
             playerBorad = (int[,])black_borad.Clone();
             enemyBorad = (int[,])white_borad.Clone();
             tmp_borad = new int[FIELD_SIZE.Height, FIELD_SIZE.Width];
@@ -229,8 +247,53 @@ namespace reversi
                     if (legal_borad[x, y] == 1) continue;
                     legal_borad[x, y] = (tmp_borad[x, y] == 1 && empty_borad[x, y] == 1) ? 1 : 0;
                 }
+            }*/
+
+            // 下
+            /*
+            playerBorad = (int[,])black_borad.Clone();
+            enemyBorad = (int[,])white_borad.Clone();
+            tmp_borad = new int[FIELD_SIZE.Height, FIELD_SIZE.Width];
+            for (int count = 0; count < FIELD_SIZE.Height - 2; count++)
+            {
+                for (int x = 0; x < FIELD_SIZE.Width; x++)
+                {
+                    // 下シフト
+                    var last = playerBorad[x, FIELD_SIZE.Height - 1];
+                    for (int y = FIELD_SIZE.Height - 1; y > 0; y--)
+                    {
+                        playerBorad[x, y] = playerBorad[x, y - 1];
+                    }
+                    playerBorad[x, 0] = last;
+
+                    // 自分の石が相手の石と同じだったら && それより左側は壁ではない
+                    for (int y = 0; y < FIELD_SIZE.Height; y++)
+                    {
+                        if (tmp_borad[x, y] == 1) continue;
+                        tmp_borad[x, y] = ((playerBorad[x, y] == 1 && enemyBorad[x, y] == 1) && y > 0) ? 1 : 0;
+                    }
+                }
             }
-            
+
+            // 下合法手
+            for (int x = 0; x < FIELD_SIZE.Width; x++)
+            {
+                // 下シフト
+                var last = tmp_borad[x, 0];
+                for (int y = FIELD_SIZE.Height - 1; y > 0; y--)
+                {
+                    tmp_borad[x, y] = tmp_borad[x, y - 1];
+                }
+                tmp_borad[x, 0] = last;
+
+                // ひっくり返せそうなマスが空マスか
+                for (int y = 0; y < FIELD_SIZE.Height; y++)
+                {
+                    if (legal_borad[x, y] == 1) continue;
+                    legal_borad[x, y] = (tmp_borad[x, y] == 1 && empty_borad[x, y] == 1) ? 1 : 0;
+                }
+            }*/
+
 
             // 配置可能マスに丸を配置
             for (int y = 0; y < FIELD_SIZE.Height; y++)
